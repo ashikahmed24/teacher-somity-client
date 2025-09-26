38a281893bc1315f920c24c1165a14d9f0736f0b
Ashik Ahmed <md.ashikahmed@outlook.com>
2025-09-26 21:38:43 +0600

    auth added

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -1,9 +1,13 @@
 <script setup>
+import BaseButton from '@/components/BaseButton.vue';
+import BaseInput from '@/components/BaseInput.vue';
 import Auth from '@/layouts/Auth.vue';
 import { useAuthStore } from '@/stores/auth';
+import { storeToRefs } from 'pinia';
 import { reactive } from 'vue';
 
 const authStore = useAuthStore();
+const { errors } = storeToRefs(authStore);
 
 const form = reactive({
     email: 'info@ashikahmed.net',
@@ -19,55 +23,64 @@ const submit = () => {
 
 <template>
     <Auth>
-        <div class="bg-white">
-            <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900">
-                    <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-                        alt="logo">
-                    Flowbite
-                </a>
-                <div
-                    class="w-full bg-gray-50 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                    <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                            Sign in to your account
-                        </h1>
-                        <form @submit.prevent="submit" class="space-y-4 md:space-y-6">
-                            <div class="form__group">
-                                <label for="email" class="form__label">Email</label>
-                                <input type="email" v-model="form.email" id="email" class="form__control"
-                                    placeholder="name@company.com" required="">
-                            </div>
-                            <div class="form__group">
-                                <label for="password" class="form__label">Password</label>
-                                <input type="password" v-model="form.password" id="password" placeholder="xxxxxxxx"
-                                    class="form__control" required="">
-                            </div>
-                            <div class="flex items-center justify-between">
-                                <div class="flex items-start">
-                                    <div class="flex items-center h-5">
-                                        <input id="remember" aria-describedby="remember" type="checkbox"
-                                            class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800">
-                                    </div>
-                                    <div class="ml-3 text-sm">
-                                        <label for="remember" class="text-gray-500 dark:text-gray-300">Remember
-                                            me</label>
-                                    </div>
-                                </div>
-                                <a href="#"
-                                    class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot
-                                    password?</a>
-                            </div>
-                            <button type="submit" class="base__button w-full">Sign
-                                in</button>
-                            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Don’t have an account yet? <a href="#"
-                                    class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign
-                                    up</a>
-                            </p>
-                        </form>
+        <div class="min-h-screen bg-gray-50 flex flex-col justify-center items-center px-4">
+            <!-- Main Card -->
+            <div class="w-full max-w-5xl bg-white rounded-lg grid grid-cols-1 md:grid-cols-2">
+
+                <!-- Left Section -->
+                <div class="flex flex-col justify-center items-center p-10">
+                    <img src="/auth.jpg" alt="Illustration" class="mb-6">
+                    <h2 class="text-2xl font-bold text-center mb-3">Show your skills be an innovator</h2>
+                    <p class="text-gray-500 text-center">
+                        Seize the moment and help shape the future by starting a career in blockchain now
+                    </p>
+                    <div class="flex justify-center mt-6 space-x-2">
+                        <span class="w-2 h-2 bg-blue-500 rounded-full"></span>
+                        <span class="w-2 h-2 bg-gray-300 rounded-full"></span>
+                        <span class="w-2 h-2 bg-gray-300 rounded-full"></span>
+                        <span class="w-2 h-2 bg-gray-300 rounded-full"></span>
                     </div>
                 </div>
+
+
+                <!-- Right Section -->
+                <div class="p-10">
+                    <h2 class="text-2xl font-bold mb-2">Sign In</h2>
+                    <p class="text-gray-500 mb-6">Welcome back, you've been missed!</p>
+
+                    <!-- Form -->
+                    <form @submit.prevent="submit">
+                        <BaseInput label="Email" v-model="form.email" placeholder="Enter your email"
+                            :error="errors.email" />
+
+                        <BaseInput label="Password" v-model="form.password" type="password" placeholder="Enter password"
+                            :error="errors.password" />
+
+                        <div class="flex items-center justify-between mb-6">
+                            <label class="flex items-center space-x-2">
+                                <input type="checkbox" class="accent-emerald-500">
+                                <span class="text-sm text-gray-600">Remember Me</span>
+                            </label>
+                            <RouterLink to="#" class="text-sm text-blue-600 hover:underline">Forget
+                                Password?</RouterLink>
+                        </div>
+
+                        <BaseButton :loading="authStore.loading" class="w-full">Submit</BaseButton>
+
+                    </form>
+
+                    <p class="text-sm text-center text-gray-600 mt-6">
+                        Don’t have an account yet?
+                        <RouterLink to="#" class="text-blue-600 hover:underline">Sign Up
+                        </RouterLink>
+                    </p>
+                </div>
+            </div>
+
+            <div class="mt-6 flex flex-wrap justify-center gap-6 text-sm text-gray-500">
+                <a href="#">Terms & Condition</a>
+                <a href="#">Privacy Policy</a>
+                <a href="#">Help</a>
             </div>
         </div>
     </Auth>
